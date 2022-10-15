6f1894bde45e8cd2b3908d78df73833258bbba4f
SamuelRnn <ricardo.reinn@outlook.com>
2022-10-14 19:54:06 -0500

    First commit

---
+++ public/app.js
@@ -0,0 +1,26 @@
+const input = document.getElementById('toDoInput');
+const addBtn = document.getElementById('add');
+let taskId = 0;
+let taskList;
+
+function createTask(val){
+    const taskDiv = document.createElement('div');
+    const taskDesc = document.createElement('p')
+
+    taskDiv.appendChild(taskDesc);
+    taskDiv.className = 'task-div';
+    taskDesc.className = 'task-description';
+    taskDiv.id = ++taskId;
+    taskDesc.innerHTML = val.value;
+    taskDiv.onclick = function(){taskDiv.remove()}
+    return taskDiv;
+}
+
+const tasksContainer = document.querySelector('.tasks-container');
+
+function addTask(){
+    if(!input.value) return;
+    tasksContainer.appendChild(createTask(input));
+    input.value = '';
+}
+addBtn.onclick = addTask;
